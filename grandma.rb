c0fb85bb085cc7ff2b5bed492197000a5dbcdae8
Sandie Nunez <snunez@vt.edu>
2020-03-30 03:46:01 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
-  elseif phrase.upcase != phrase
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase.upcase != phrase
     return "HUH?! SPEAK UP, SONNY!"
-  
-    if(phrase == "I Love you grandma!".upcase) #TRUE shouting TRUE love
-     return "I LOVE YOU TOO PUMPKIN!"
-    elseif #TRUE shouting !TRUE love
-     retun "NO, NOT SINCE 1938!"
-    end
-  else 
-    return "HUH?! SPEAK UP, SONNY"
+  else
+    return "NO, NOT SINCE 1938!"
   end
+
 end
