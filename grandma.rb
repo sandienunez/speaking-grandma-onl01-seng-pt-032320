35dd10d2f924e51b8a16670ac99129c34a4fe6d5
Sandie Nunez <snunez@vt.edu>
2020-03-30 03:44:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   elsof phrase.upcase != phrase
     return "HUH?! SPEAK UP, SONNY!"
   else
